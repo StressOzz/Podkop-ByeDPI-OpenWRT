efd6c974fb56c18409ee0bbe1fc90c9bfd35e236
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-29 05:06:00 +0500

    Update Podkop-Manager.sh

--- Podkop-Manager.sh
+++ Podkop-Manager.sh
@@ -66,7 +66,7 @@ if ! curl --version >/dev/null 2>&1; then echo -e "\ncurl ${RED}отсутств
 $DELETE curl libcurl >/dev/null 2>&1; echo -e "${CYAN}Обновляем список пакетов${NC}"; if ! $UPDATE >/dev/null 2>&1; then echo -e "\n${RED}Ошибка обновления списка пакетов!${NC}\n"; else PACKAGES_UPDATED=1; fi
 echo -e "${CYAN}Устанавливаем ${NC}curl"; if ! $INSTALL libcurl curl >/dev/null 2>&1; then echo -e "\n${RED}Не удалось установить curl!${NC}\n"; PAUSE; fi; fi
 
-PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/itdoginfo/podkop/releases/latest | sed -E 's#.*/tag/v?##')"
+PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' ${GH_MAIN}/itdoginfo/podkop/releases/latest | sed -E 's#.*/tag/v?##')"
 
 BYEDPI_VER="0.17.3"
 
@@ -645,16 +645,50 @@ PAUSE
 }
 
 # ==========================================
+# Зеркало
+# ==========================================
+set_mirror() { NEW_BASE="$1"; echo -e "\n${CYAN}Проверяем доступность ${NC}$NEW_BASE"
+if ! wget -q --spider --timeout=5 "https://$NEW_BASE/releases/" >/dev/null 2>&1; then echo -e "${RED}Зеркало недоступно!${NC}\n"; PAUSE; return 1; fi
+sed -i "s|https://.*/releases/|https://$NEW_BASE/releases/|g" "$CONFZ"; echo -e "${GREEN}Зеркало доступно!${NC}\n${CYAN}Обновляем список пакетов${NC}"
+if ! $UPDATE >/dev/null 2>&1; then echo -e "\n${RED}Ошибка обновления списка пакетов!${NC}\n${GREEN}Зеркало сброшено на ${NC}default ${GREEN}/${NC} OpenWrt${GREEN}!${NC}\n"
+sed -i "s|https://.*/releases/|https://downloads.openwrt.org/releases/|g" "$CONFZ"; PAUSE; return 1; fi; echo -e "${GREEN}Пакеты обновлены! Зеркало работает!${NC}\n"; PAUSE; }
+
+
+curr_MIR() { if [ -f "$CONFZ" ]; then URL=$(head -n1 "$CONFZ"); case "$URL" in
+*mirror-03.infra.openwrt.org*) echo "infra.openwrt.org" ;;
+*tiguinet.net*) echo "Belgium" ;;
+*utwente.nl*) echo "Netherlands" ;;
+*freifunk.net*) echo "Germany" ;;
+*ps.kz*) echo "Kazakhstan" ;;
+*sjtu.edu.cn*) echo "China" ;;
+*downloads.openwrt.org*) echo "default / OpenWrt" ;;
+*) echo "неизвестное" ;;
+esac; else echo "файл не найден"; fi; }
+
+menu_MIR() { while true; do clear; CURR=$(curr_MIR); echo -e "${MAGENTA}Меню выбора зеркала OpenWrt${NC}\n\n${YELLOW}Используется зеркало: ${GREEN}$CURR${NC}\n\n${CYAN}1)${NC} infra.openwrt.org\n${CYAN}2)${NC} China"
+echo -e "${CYAN}3)${NC} Germany\n${CYAN}4)${NC} Belgium\n${CYAN}5)${NC} Kazakhstan\n${CYAN}6)${NC} Netherlands\n${CYAN}7)${NC} default / OpenWrt${NC}"
+echo -en "\n${YELLOW}Выберите зеркало: ${NC}"; read -r z; case "$z" in
+1) set_mirror "mirror-03.infra.openwrt.org" ;;
+2) set_mirror "mirror.sjtu.edu.cn/openwrt" ;;
+3) set_mirror "mirror.berlin.freifunk.net/downloads.openwrt.org" ;;
+4) set_mirror "mirror.tiguinet.net/openwrt" ;;
+5) set_mirror "mirror.ps.kz/openwrt" ;;
+6) set_mirror "ftp.snt.utwente.nl/pub/software/openwrt" ;;
+7) set_mirror "downloads.openwrt.org" ;;
+*) break ;;
+esac; done; }
+
+# ==========================================
 # Меню
 # ==========================================
 show_menu() {
-get_versions
+get_versions; CURR=$(curr_MIR)
 
 clear
 echo -e "╔═══════════════════════════════╗"
 echo -e "║         ${BLUE}Podkop Manager${NC}        ║"
 echo -e "╚═══════════════════════════════╝"
-echo -e "                ${DGRAY}by StressOzz v2.9${NC}"
+echo -e "                ${DGRAY}by StressOzz v3.0${NC}"
 
 echo -e "${MAGENTA}--- Podkop ---${NC}"
 echo -e "${YELLOW}Установленная версия:${NC} $PODKOP_STATUS"
@@ -673,6 +707,8 @@ if uci -q get network.AWG >/dev/null; then
 else
     echo -e "${YELLOW}Интерфейс AWG: ${RED}не установлен${NC}"
 fi
+echo -e "${MAGENTA}--- зеркало OpenWRT ---${NC}"
+echo -e "${YELLOW}Зеркало: ${GREEN}$CURR${NC}"
 
 echo -e "\n${CYAN}1) ${GREEN}Установить ${NC}Podkop"
 echo -e "${CYAN}2) ${GREEN}Удалить ${NC}Podkop"
@@ -683,7 +719,8 @@ echo -e "${CYAN}6) ${GREEN}Изменить стратегию ${NC}ByeDPI"
 echo -e "${CYAN}7) ${GREEN}Установить ${NC}AWG ${GREEN}и${NC} интерфейс AWG"
 echo -e "${CYAN}8) ${GREEN}Удалить ${NC}AWG ${GREEN}и${NC} интерфейс AWG"
 echo -e "${CYAN}9) ${GREEN}Интегрировать ${NC}AWG ${GREEN}в ${NC}Podkop"
-echo -e "${CYAN}0) ${GREEN}Перезагрузить устройство${NC}"
+echo -e "${CYAN}0) ${GREEN}Меню выбора зеркала ${NC}OpenWrt"
+echo -e "${CYAN}r) ${GREEN}Перезагрузить устройство${NC}"
 echo -e "${CYAN}Enter) ${GREEN}Выход${NC}"
 echo -ne "\n${YELLOW}Выберите пункт:${NC} "
 read choice
@@ -698,7 +735,8 @@ case "$choice" in
 7) install_AWG ;;
 8) uninstall_AWG ;;
 9) integration_AWG ;;
-0) echo -e "\n${GREEN}Перезагрузка!${NC}\n"; reboot; exit 0 ;;
+0) menu_MIR ;;
+r|R|к|К) echo -e "\n${GREEN}Перезагрузка!${NC}\n"; reboot; exit 0 ;;
 *) exit 0 ;;
 esac
 }
